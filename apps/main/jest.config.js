6c66d80185f22f3a7bf5567659b365e9f679574d
yutaro_elk <36399275+yutaro-elk@users.noreply.github.com>
2021-12-03 21:53:50 +0900

    rename client -> main

--- apps/main/jest.config.js
+++ apps/main/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'client',
+  displayName: 'main',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/client',
+  coverageDirectory: '../../coverage/apps/main',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
